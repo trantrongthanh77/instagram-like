861867e5c62cac1b01826612292640e09cee0334
Tran Trong Thanh <thanhtt@cozwork.com>
2021-11-01 08:37:31 +0700

    fix bug need to token to register user

--- src/main/java/com/cozwork/config/ResourceServerConfiguration.java
+++ src/main/java/com/cozwork/config/ResourceServerConfiguration.java
@@ -16,6 +16,7 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/image/image-name/**").permitAll()
+                .antMatchers("/user/add-user").permitAll()
                 .anyRequest().authenticated();
     }
 }
